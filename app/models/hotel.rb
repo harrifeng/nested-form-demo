110d5bfc10064e29f5c5971c411029cac63db52a
Haoran Feng <haoran.feng@alo7.com>
2016-11-21 19:36:00 +0800

    destroy

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -2,7 +2,8 @@ class Hotel < ApplicationRecord
   has_many :room_categories, dependent: :destroy
 
   accepts_nested_attributes_for :room_categories,
-                                reject_if: proc { |attributes| attributes['name'].blank? }
+                                reject_if: proc { |attributes| attributes['name'].blank? },
+                                allow_destroy: true
 
 
   validates :name,
