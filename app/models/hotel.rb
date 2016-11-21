b62d8cfe1d2f31c99d8cf55fe09d3ab5939fd069
Haoran Feng <haoran.feng@alo7.com>
2016-11-21 18:27:31 +0800

    can edit, only edit

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -1,5 +1,8 @@
 class Hotel < ApplicationRecord
   has_many :room_categories, dependent: :destroy
+
+  accepts_nested_attributes_for :room_categories
+
   validates :name,
             presence: true
 
