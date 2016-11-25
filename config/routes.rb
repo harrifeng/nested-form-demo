20a5dff50373bc049d200e3206c34288d7b3af82
Haoran Feng <haoran.feng@alo7.com>
2016-11-25 17:15:13 +0800

    new index page set

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
       post :update_multiple
     end
   end
+
+  root 'hotels#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
